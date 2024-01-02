23b17625471a38c4543672e83a3b83b748fcf14f
Moises Alarcon <moiseshazim@gmail.com>
2024-01-02 11:37:54 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Actividad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar el ejercicio descrito en este apartado:
+            string nombre = "Moises";
+            string apellido = "Alarcon";
+            int edad = 24;
+            string sexo = "Masculino";
+            string estado_civil = "Soltero";
+            string telefono = "829-968-8949";
+            string direccion = "San Pedro De Macoris";
+            string email = "moises.alarcon@unicaribe.edu.do";
+            string estatus = "ONLINE(ACTIVO)";
+            Console.Write("Los siguientes datos son los que estan almacenados en la variable: " + '\n');
+            Console.WriteLine("Su nombre es " + nombre
+                + " " + apellido + '\n' 
+                + "Tiene " + edad + '\n' +
+             "Sexo: " +sexo  + '\n' + 
+             "Su estado civil es: " + estado_civil + '\n' +
+             "Su direccion es: "  + direccion  + '\n' +
+             "Su email es: " + email + '\n' + 
+             "Status: " + estatus  + '\n'
+             );
+
+        }
+    }
+}
